dba770a897fa3dcdaa33fefdf286570a906aa950
dsibagat <Damir.Sibagatulin@t-systems.com>
2021-02-23 15:29:15 +0300

    added selenoid support

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,30 +12,31 @@ import static helpers.AttachmentsHelper.*;
 public class TestBase {
     @BeforeAll
     static void setup() {
-        addListener("allure", new AllureSelenide());
-        Configuration.browser = System.getProperty("browser", "chrome");
-
-        if (System.getProperty("remote_driver") != null) {
-            // config for Java + Selenide
+      addListener("allure", new AllureSelenide());
+//        Configuration.browser = System.getProperty("browser", "chrome");
+//
+//        if (System.getProperty("remote_driver") != null) {
+//            // config for Java + Selenide
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-            Configuration.remote = System.getProperty("remote_driver");
+//            capabilities.setCapability("enableVNC", true);
+//            capabilities.setCapability("enableVideo", true);
+//            Configuration.browserCapabilities = capabilities;
+         //  Configuration.remote = System.getProperty("remote_driver");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
 
-            // config for Java + Selenium
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("browserName", "chrome");
-//        capabilities.setCapability("browserVersion", "87.0");
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//        RemoteWebDriver driver = new RemoteWebDriver(
-//                URI.create("http://selenoid:4444/wd/hub").toURL(),
-//                capabilities
-//        );
-        }
+//            // config for Java + Selenium
+////        DesiredCapabilities capabilities = new DesiredCapabilities();
+////        capabilities.setCapability("browserName", "chrome");
+////        capabilities.setCapability("browserVersion", "87.0");
+////        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+////                "enableVNC", true,
+////                "enableVideo", true
+////        ));
+////        RemoteWebDriver driver = new RemoteWebDriver(
+////                URI.create("http://selenoid:4444/wd/hub").toURL(),
+////                capabilities
+////        );
+//        }
     }
 
     @AfterEach
